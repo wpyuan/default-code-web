9d923210cd2d7efdad673ed6847672792708f5bf
wangpeiyuan <490176245@qq.com>
2021-05-10 16:34:02 +0800

    初始化

---
+++ src/main/java/com/github/wpyuan/defaultcodeweb/DefaultCodeWebApplication.java
@@ -0,0 +1,13 @@
+package com.github.wpyuan.defaultcodeweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefaultCodeWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DefaultCodeWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/wpyuan/defaultcodeweb/DefaultCodeWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.wpyuan.defaultcodeweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DefaultCodeWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
